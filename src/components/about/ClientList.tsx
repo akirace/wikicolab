bc56662b642e6ba680841db2b2dc7327c6c0fcf3
Dicky Arya Pratama <dickyarya.prtma@gmail.com>
2025-07-14 13:20:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card className="w

--- src/components/about/ClientList.tsx
+++ src/components/about/ClientList.tsx
@@ -16,7 +16,7 @@ const clients = [
     logo: "https://img.aghatis.id/cdn/bG9jYWw6Ly8vbG9nb19iYW5rX25lZ2FyYV9pbmRvbmVzaWEucG5n" 
   },
   { name: "PT Wina Mitra Corpora", logo: "https://img.aghatis.id/cdn/bG9jYWw6Ly8vcHRfcGVuZ2FkYWFuLnBuZw==" },
-  { name: "PT Sifang System Indonesia", logo: null }
+  { name: "PT Sifang System Indonesia", logo: "https://img.aghatis.id/cdn/bG9jYWw6Ly8vcHRfc2lmYW5nLnBuZw==" }
 ];
 
 export function ClientList() {
